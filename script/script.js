3d4d4ea1724ba35ff99946e47da799abefebcce7
Gustavo Silveira Nicoletti <gus.nicoletti@gmail.com>
2025-12-03 15:53:38 -0300

    corrigindo encode do js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("whatsappForm");
 
 form.addEventListener("submit", function(e) {
   e.preventDefault()
-  const mensagem = encodeURIComponent(document.getElementById("mensagem").value)
+  const mensagem = document.getElementById("mensagem").value
   const url = `https://wa.me/5581985550906?text=${encodeURIComponent(mensagem)}`;
   window.open(url, "_blank")
 });
